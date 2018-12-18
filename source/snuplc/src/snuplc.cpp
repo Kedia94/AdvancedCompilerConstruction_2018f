ebfc408387bfa528a2fee0bd43214a69c23c3ec8
kedia94 <kedia94@gmail.com>
2018-12-18 23:38:58 +0000

    change output file
    git-svn-id: https://snupl.snu.ac.kr/svn/team5@33 59736999-5421-4a3c-acc9-5c193985b34f

--- source/snuplc/src/snuplc.cpp
+++ source/snuplc/src/snuplc.cpp
@@ -50,7 +50,7 @@
 using namespace std;
 
 #define CONST
-#define DEAD
+//#define DEAD
 #define INLINE
 
 bool dump_ast = true;
@@ -65,464 +65,337 @@ vector<string> files;
 
 void Syntax(string msg)
 {
-  if (msg != "") cout << msg << endl << endl;
+	if (msg != "") cout << msg << endl << endl;
 
-  cout << "Usage: snuplc [OPTIONS] FILES..." << endl
-       << "Compile each source file in FILES using OPTIONS." << endl
-       << "Example: snuplc fibonacci.mod" << endl
-       << endl
-       << "Options:" << endl
-       << "  --ast          output the AST in textual/graphical form. Default: off" << endl
-       << "  --tac          output the IR in textual/graphical form. Default: off" << endl
-       << "  --exe          generate executable from compiled assembly file. Default: off" << endl
-       << "  --no-asm       output assembly code to console instead of a file. Default: file" << endl
-       << "  --no-dot       do not output the AST/IR in graphical form. Default: output in graphical form" << endl
-       << "  --no-run-dot   do not run the dot command automatically. Default: run automatically" << endl
-       << endl
-       << endl
-       << "Examples:" << endl
-       << "  compile fibonacci.mod and write generated assembly code to fibonacci.mod.s" << endl
-       << "  $ snuplc fibonacci.mod" << endl
-       << endl
-       << "  compile fibonacci.mod to fibonacci.mod.s, then generate fibonacci executable" << endl
-       << "  $ snuplc --exe fibonacci.mod" << endl
-       << endl
-       << "  compile fibonacci.mod and also output the AST in textual and graphical form" << endl
-       << "  The AST is saved in fibonacci.mod.ast (textual) and fibonacci.mod.ast.dot (graphical form)" << endl
-       << "  $ snuplc --ast fibonacci.mod" << endl
-       << endl
-       << "  compile fibonacci.mod and also output the IR in textual and graphical form" << endl
-       << "  The IR is saved in fibonacci.mod.tac (textual) and fibonacci.mod.tac.dot (graphical form)" << endl
-       << "  $ snuplc --tac fibonacci.mod" << endl
-       << endl;
+	cout << "Usage: snuplc [OPTIONS] FILES..." << endl
+		<< "Compile each source file in FILES using OPTIONS." << endl
+		<< "Example: snuplc fibonacci.mod" << endl
+		<< endl
+		<< "Options:" << endl
+		<< "  --ast          output the AST in textual/graphical form. Default: off" << endl
+		<< "  --tac          output the IR in textual/graphical form. Default: off" << endl
+		<< "  --exe          generate executable from compiled assembly file. Default: off" << endl
+		<< "  --no-asm       output assembly code to console instead of a file. Default: file" << endl
+		<< "  --no-dot       do not output the AST/IR in graphical form. Default: output in graphical form" << endl
+		<< "  --no-run-dot   do not run the dot command automatically. Default: run automatically" << endl
+		<< endl
+		<< endl
+		<< "Examples:" << endl
+		<< "  compile fibonacci.mod and write generated assembly code to fibonacci.mod.s" << endl
+		<< "  $ snuplc fibonacci.mod" << endl
+		<< endl
+		<< "  compile fibonacci.mod to fibonacci.mod.s, then generate fibonacci executable" << endl
+		<< "  $ snuplc --exe fibonacci.mod" << endl
+		<< endl
+		<< "  compile fibonacci.mod and also output the AST in textual and graphical form" << endl
+		<< "  The AST is saved in fibonacci.mod.ast (textual) and fibonacci.mod.ast.dot (graphical form)" << endl
+		<< "  $ snuplc --ast fibonacci.mod" << endl
+		<< endl
+		<< "  compile fibonacci.mod and also output the IR in textual and graphical form" << endl
+		<< "  The IR is saved in fibonacci.mod.tac (textual) and fibonacci.mod.tac.dot (graphical form)" << endl
+		<< "  $ snuplc --tac fibonacci.mod" << endl
+		<< endl;
 
-  exit(EXIT_FAILURE);
+	exit(EXIT_FAILURE);
 }
 
 void ParseArgs(int argc, char *argv[])
 {
-  //files.push_back(string("test10.mod"));
+	//files.push_back(string("test10.mod"));
 
-  int i = 1;
+	int i = 1;
 
-  while (i < argc) {
-    if ((strlen(argv[i]) >= 2) && (argv[i][0] == '-') && (argv[i][1] == '-')) {
-      if (strcmp(argv[i], "--ast") == 0) dump_ast = true;
-      else if (strcmp(argv[i], "--tac") == 0) dump_tac = true;
-      else if (strcmp(argv[i], "--no-asm") == 0) dump_asm = false;
-      else if (strcmp(argv[i], "--no-dot") == 0) dump_dot = false;
-      else if (strcmp(argv[i], "--no-run-dot") == 0) run_dot = false;
-      else if (strcmp(argv[i], "--exe") == 0) run_gcc = true;
-      else if (strcmp(argv[i], "--rte") == 0) {
-        i++;
-        if (i == argc) Syntax("Missing argument after --rte");
-        rte_path = string(argv[i]);
-      }
-      else if (strcmp(argv[i], "--help") == 0) Syntax("");
-      else Syntax("Unknown command line option '" + string(argv[i]) + "'.");
-    }
-    else 
-    
-    files.push_back(string(argv[i]));
-    i++;
-  }
- 
+	while (i < argc) {
+		if ((strlen(argv[i]) >= 2) && (argv[i][0] == '-') && (argv[i][1] == '-')) {
+			if (strcmp(argv[i], "--ast") == 0) dump_ast = true;
+			else if (strcmp(argv[i], "--tac") == 0) dump_tac = true;
+			else if (strcmp(argv[i], "--no-asm") == 0) dump_asm = false;
+			else if (strcmp(argv[i], "--no-dot") == 0) dump_dot = false;
+			else if (strcmp(argv[i], "--no-run-dot") == 0) run_dot = false;
+			else if (strcmp(argv[i], "--exe") == 0) run_gcc = true;
+			else if (strcmp(argv[i], "--rte") == 0) {
+				i++;
+				if (i == argc) Syntax("Missing argument after --rte");
+				rte_path = string(argv[i]);
+			}
+			else if (strcmp(argv[i], "--help") == 0) Syntax("");
+			else Syntax("Unknown command line option '" + string(argv[i]) + "'.");
+		}
+		else 
+
+			files.push_back(string(argv[i]));
+		i++;
+	}
+
 }
 
 void RunDOT(string file)
 {
-  if (run_dot) {
-    ostringstream cmd;
-    cmd << "dot -Tpdf -o" << file << ".pdf " << file;
+	if (run_dot) {
+		ostringstream cmd;
+		cmd << "dot -Tpdf -o" << file << ".pdf " << file;
 
-    cout << "  running command '" << cmd.str() << "'..." << endl;
-    if (system(cmd.str().c_str()) < 0) {
-      cout << "  failed to run dot." << endl;
-    }
-  }
+		cout << "  running command '" << cmd.str() << "'..." << endl;
+		if (system(cmd.str().c_str()) < 0) {
+			cout << "  failed to run dot." << endl;
+		}
+	}
 }
 
 void RunCompile(string file)
 {
-  if (run_gcc) {
-    ostringstream cmd;
+	if (run_gcc) {
+		ostringstream cmd;
 
-    string exe(file);
-    exe.erase(exe.find(".mod"));
+		string exe(file);
+		exe.erase(exe.find(".mod"));
 
-    cmd << "gcc -m32 -o" << exe << " "
-        << rte_path << "IO.s" << " "
-        << rte_path << "ARRAY.s" << " "
-        << file;
+		cmd << "gcc -m32 -o" << exe << " "
+			<< rte_path << "IO.s" << " "
+			<< rte_path << "ARRAY.s" << " "
+			<< file;
 
-    cout << "  running command '" << cmd.str() << "'..." << endl;
-    if (system(cmd.str().c_str()) < 0) {
-      cout << "  failed to run gcc." << endl;
-    }
-  }
+		cout << "  running command '" << cmd.str() << "'..." << endl;
+		if (system(cmd.str().c_str()) < 0) {
+			cout << "  failed to run gcc." << endl;
+		}
+	}
 }
 
 void DumpAST(string file, CAstModule *ast)
 {
-  if (dump_ast) {
-    assert(ast != NULL);
+	if (dump_ast) {
+		assert(ast != NULL);
 
-    // output AST in textual form
-    ofstream out(file + ".ast");
-    out << file << ":" << endl;
-    ast->print(out, 4);
-    out << endl << endl
-      << "  symbol table:" << endl;
-    ast->GetSymbolTable()->print(out, 4);
-    out << endl;
+		// output AST in textual form
+		ofstream out(file + ".ast");
+		out << file << ":" << endl;
+		ast->print(out, 4);
+		out << endl << endl
+			<< "  symbol table:" << endl;
+		ast->GetSymbolTable()->print(out, 4);
+		out << endl;
 
-    // output AST in graphical form
-    if (dump_dot) {
-      string fn = file + ".ast.dot";
-      ofstream dot(fn);
-      dot << "digraph AST {" << endl
-          << "  graph [fontname=\"Times New Roman\",fontsize=10];" << endl
-          << "  node  [fontname=\"Courier New\",fontsize=10];" << endl
-          << "  edge  [fontname=\"Times New Roman\",fontsize=10];" << endl
-          << endl;
-      ast->toDot(dot, 2);
-      dot << "}" << endl;
-      dot.flush();
+		// output AST in graphical form
+		if (dump_dot) {
+			string fn = file + ".ast.dot";
+			ofstream dot(fn);
+			dot << "digraph AST {" << endl
+				<< "  graph [fontname=\"Times New Roman\",fontsize=10];" << endl
+				<< "  node  [fontname=\"Courier New\",fontsize=10];" << endl
+				<< "  edge  [fontname=\"Times New Roman\",fontsize=10];" << endl
+				<< endl;
+			ast->toDot(dot, 2);
+			dot << "}" << endl;
+			dot.flush();
 
-      RunDOT(fn);
-    }
-  }
+			RunDOT(fn);
+		}
+	}
 }
 
 void DumpTAC(string file, CModule *m)
 {
-  if (dump_tac) {
-    assert(m != NULL);
+	if (dump_tac) {
+		assert(m != NULL);
 
-    // output TAC in textual form
-    ofstream out(file + ".tac");
-    out << file << ":" << endl
-        << m << endl;
+		// output TAC in textual form
+		ofstream out(file + ".tac");
+		out << file << ":" << endl
+			<< m << endl;
 
-    // output TAC in graphical form
-    if (dump_dot) {
-      string fn = file + ".tac.dot";
-      ofstream dot(fn);
+		// output TAC in graphical form
+		if (dump_dot) {
+			string fn = file + ".tac.dot";
+			ofstream dot(fn);
 
-      dot << "digraph IR {" << endl
-          << "  graph [fontname=\"Times New Roman\",fontsize=10];" << endl
-          << "  node  [fontname=\"Courier New\",fontsize=10];" << endl
-          << "  edge  [fontname=\"Times New Roman\",fontsize=10];" << endl
-          << endl;
-      m->toDot(dot, 2);
-      const vector<CScope*> &proc = m->GetSubscopes();
-      for (size_t p=0; p<proc.size(); p++) {
-        proc[p]->toDot(dot, 2);
-      }
-      dot<< "}" << endl;
-      dot.flush();
+			dot << "digraph IR {" << endl
+				<< "  graph [fontname=\"Times New Roman\",fontsize=10];" << endl
+				<< "  node  [fontname=\"Courier New\",fontsize=10];" << endl
+				<< "  edge  [fontname=\"Times New Roman\",fontsize=10];" << endl
+				<< endl;
+			m->toDot(dot, 2);
+			const vector<CScope*> &proc = m->GetSubscopes();
+			for (size_t p=0; p<proc.size(); p++) {
+				proc[p]->toDot(dot, 2);
+			}
+			dot<< "}" << endl;
+			dot.flush();
 
-      RunDOT(fn);
-    }
-  }
+			RunDOT(fn);
+		}
+	}
 }
 
 int main(int argc, char *argv[])
 {
-  ParseArgs(argc, argv);
+	ParseArgs(argc, argv);
 
-  vector<string>::const_iterator it = files.begin();
+	vector<string>::const_iterator it = files.begin();
 
-  if (it == files.end()) Syntax("No input files.");
+	if (it == files.end()) Syntax("No input files.");
 
-  while (it != files.end()) {
-    string file = *it++;
+	while (it != files.end()) {
+		string file = *it++;
 
-    // scanning, parsing & semantical analysis
-    CScanner *s = new CScanner(new ifstream(file));
-    CParser *p = new CParser(s);
+		// scanning, parsing & semantical analysis
+		CScanner *s = new CScanner(new ifstream(file));
+		CParser *p = new CParser(s);
 
-    cout << "compiling " << file << "..." << endl;
-    CAstNode *ast = p->Parse();
+		cout << "compiling " << file << "..." << endl;
+		CAstNode *ast = p->Parse();
 
-    if (p->HasError()) {
-      const CToken *error = p->GetErrorToken();
-      cout << "parse error at " << error->GetLineNumber() << ":"
-           << error->GetCharPosition() << " : "
-           << p->GetErrorMessage() << endl;
-    } else {
-      DumpAST(file, dynamic_cast<CAstModule*>(ast));
+		if (p->HasError()) {
+			const CToken *error = p->GetErrorToken();
+			cout << "parse error at " << error->GetLineNumber() << ":"
+				<< error->GetCharPosition() << " : "
+				<< p->GetErrorMessage() << endl;
+		} else {
+			DumpAST(file, dynamic_cast<CAstModule*>(ast));
 
-      // AST to TAC conversion
-      CModule *m = new CModule(ast);
+			// AST to TAC conversion
+			CModule *m = new CModule(ast);
 
-      DumpTAC(file, m);
-	  
-	  // Start DEBUG
-	  CBackend *be;
-	  ostream *out = &cout;
-	  ofstream *sout = NULL;
+			DumpTAC(file, m);
 
-	  /* Original */
-	  if (dump_asm) {
-		  sout = new ofstream(file + ".s");
-		  out = sout;
-	  }
+			// Start DEBUG
+			CBackend *be;
+			ostream *out = &cout;
+			ofstream *sout = NULL;
 
-	  be = new CBackendx86(*out);
-	  be->Emit(m);
+			/* Original */
+			if (dump_asm) {
+				sout = new ofstream(file + ".s");
+				out = sout;
+			}
 
-	  if (sout != NULL) {
-		  sout->flush();
-		  delete sout;
-	  }
+			be = new CBackendx86(*out);
+			be->Emit(m);
 
-	  RunCompile(file + ".s");
-	  /* Original */
+			if (sout != NULL) {
+				sout->flush();
+				delete sout;
+			}
+
+			RunCompile(file + ".s");
+			/* Original */
 #ifdef CONST
-	  {
-		  ConstantP *b = new ConstantP(m);
-		  b->doConstantPropagation();
-		  delete b;
-		  DumpTAC("test_const", m);
+			{
+				ConstantP *b = new ConstantP(m);
+				b->doConstantPropagation();
+				delete b;
 
-		  if (dump_asm) {
-			  sout = new ofstream("test_const.s");
-			  out = sout;
-		  }
-
-		  be = new CBackendx86(*out);
-		  be->Emit(m);
-
-		  if (sout != NULL) {
-			  sout->flush();
-			  delete sout;
-		  }
-
-		  RunCompile("test_const.s");
-	  }
+			}
 #endif
-	  /* Original */
+			/* Original */
 #ifdef DEAD
-	  {
-		  DeadCodeE *d = new DeadCodeE(m);
-		  d->eliminateDeadCode();
-		  delete d;
+			{
+				DeadCodeE *d = new DeadCodeE(m);
+				d->eliminateDeadCode();
+				delete d;
 
-		  // End DEBUG
-		  DumpTAC("test_dead", m);
+				// End DEBUG
 
-		  // output x86 assembly to console or file
-
-		  if (dump_asm) {
-			  sout = new ofstream("test_dead.s");
-			  out = sout;
-		  }
-
-		  be = new CBackendx86(*out);
-		  be->Emit(m);
-
-		  if (sout != NULL) {
-			  sout->flush();
-			  delete sout;
-		  }
-
-		  RunCompile("test_dead.s");
-	  }
+				// output x86 assembly to console or file
+			}
 #endif
 #ifdef CONST
-	  {
-		  ConstantP *b = new ConstantP(m);
-		  b->doConstantPropagation();
-		  delete b;
-		  DumpTAC("test_const", m);
-
-		  if (dump_asm) {
-			  sout = new ofstream("test_const.s");
-			  out = sout;
-		  }
-
-		  be = new CBackendx86(*out);
-		  be->Emit(m);
-
-		  if (sout != NULL) {
-			  sout->flush();
-			  delete sout;
-		  }
-
-		  RunCompile("test_const.s");
-	  }
+			{
+				ConstantP *b = new ConstantP(m);
+				b->doConstantPropagation();
+				delete b;
+			}
 #endif
-	  /* Original */
+			/* Original */
 #ifdef DEAD
-	  {
-		  DeadCodeE *d = new DeadCodeE(m);
-		  d->eliminateDeadCode();
-		  delete d;
+			{
+				DeadCodeE *d = new DeadCodeE(m);
+				d->eliminateDeadCode();
+				delete d;
 
-		  // End DEBUG
-		  DumpTAC("test_dead1", m);
+				// End DEBUG
 
-		  // output x86 assembly to console or file
+				// output x86 assembly to console or file
 
-		  if (dump_asm) {
-			  sout = new ofstream("test_dead.s");
-			  out = sout;
-		  }
-
-		  be = new CBackendx86(*out);
-		  be->Emit(m);
-
-		  if (sout != NULL) {
-			  sout->flush();
-			  delete sout;
-		  }
-
-		  RunCompile("test_dead.s");
-	  }
+			}
 #endif
 #ifdef INLINE
-	  
-	  Inlining *a = new Inlining();
-	  a->parseTAC(m);
-	  a->calculateScore();
-//	  cout << a << endl;
 
-	  int inlinenum=0;
-	  while (a->doOneStep() == 0)
-	  {
-		  inlinenum ++;
-//	  	cout<<inlinenum<<" ==================================="<<endl;
-//	  	cout << a << endl;
-	  }
-	  cout<<"Inlined "<<inlinenum<<" Times"<<endl;
-	  DumpTAC("test_inline", m);
-	  delete a;
+			Inlining *a = new Inlining();
+			a->parseTAC(m);
+			a->calculateScore();
+			//	  cout << a << endl;
 
-	  if (dump_asm) {
-		  sout = new ofstream("test_inline.s");
-		  out = sout;
-	  }
+			int inlinenum=0;
+			while (a->doOneStep() == 0)
+			{
+				inlinenum ++;
+				//	  	cout<<inlinenum<<" ==================================="<<endl;
+				//	  	cout << a << endl;
+			}
+			cout<<"Inlined "<<inlinenum<<" Times"<<endl;
 
-	  be = new CBackendx86(*out);
-	  be->Emit(m);
-
-	  if (sout != NULL) {
-		  sout->flush();
-		  delete sout;
-	  }
-
-	  RunCompile("test_inline.s");
 #endif
 
-	  /* const */
+			/* const */
 #ifdef CONST
-	  {
-		  ConstantP *b = new ConstantP(m);
-		  b->doConstantPropagation();
-		  delete b;
-		  DumpTAC("test_const", m);
+			{
+				ConstantP *b = new ConstantP(m);
+				b->doConstantPropagation();
+				delete b;
 
-		  if (dump_asm) {
-			  sout = new ofstream("test_const.s");
-			  out = sout;
-		  }
-
-		  be = new CBackendx86(*out);
-		  be->Emit(m);
-
-		  if (sout != NULL) {
-			  sout->flush();
-			  delete sout;
-		  }
-
-		  RunCompile("test_const.s");
-	  }
+			}
 #endif
-	  /* Original */
+			/* Original */
 #ifdef DEAD
-	  {
-		  DeadCodeE *d = new DeadCodeE(m);
-		  d->eliminateDeadCode();
-		  delete d;
-
-		  // End DEBUG
-		  DumpTAC("test_dead", m);
-
-		  // output x86 assembly to console or file
-
-		  if (dump_asm) {
-			  sout = new ofstream("test_dead.s");
-			  out = sout;
-		  }
-
-		  be = new CBackendx86(*out);
-		  be->Emit(m);
-
-		  if (sout != NULL) {
-			  sout->flush();
-			  delete sout;
-		  }
-
-		  RunCompile("test_dead.s");
-	  }
+			{
+				DeadCodeE *d = new DeadCodeE(m);
+				d->eliminateDeadCode();
+				delete d;
+			}
 #endif
 #ifdef CONST
-	  {
-		  ConstantP *b = new ConstantP(m);
-		  b->doConstantPropagation();
-		  delete b;
-		  DumpTAC("test_const", m);
-
-		  if (dump_asm) {
-			  sout = new ofstream("test_const.s");
-			  out = sout;
-		  }
-
-		  be = new CBackendx86(*out);
-		  be->Emit(m);
-
-		  if (sout != NULL) {
-			  sout->flush();
-			  delete sout;
-		  }
-
-		  RunCompile("test_const.s");
-	  }
+			{
+				ConstantP *b = new ConstantP(m);
+				b->doConstantPropagation();
+				delete b;
+			}
 #endif
-	  /* Original */
+			/* Original */
 #ifdef DEAD
-	  {
-		  DeadCodeE *d = new DeadCodeE(m);
-		  d->eliminateDeadCode();
-		  delete d;
+			{
+				DeadCodeE *d = new DeadCodeE(m);
+				d->eliminateDeadCode();
+				delete d;
+			}
+#endif
+			string substrname = file.substr(0,file.find('.'));
+//			cout<<substrname<<endl;i
+			DumpTAC(substrname+"_opt", m);
 
-		  // End DEBUG
-		  DumpTAC("test_dead", m);
-
-		  // output x86 assembly to console or file
-
-		  if (dump_asm) {
-			  sout = new ofstream("test_dead.s");
-			  out = sout;
-		  }
-
-		  be = new CBackendx86(*out);
-		  be->Emit(m);
-
-		  if (sout != NULL) {
-			  sout->flush();
-			  delete sout;
-		  }
-
-		  RunCompile("test_dead.s");
-	  }
+#ifdef INLINE
+			delete a;
 #endif
 
-	  delete be;
-	  delete m;
+			if (dump_asm) {
+				sout = new ofstream(substrname+"_opt.s");
+				out = sout;
+			}
+
+			be = new CBackendx86(*out);
+			be->Emit(m);
+
+			if (sout != NULL) {
+				sout->flush();
+				delete sout;
+			}
+
+			RunCompile(substrname+"_opt");
+
+			delete be;
+			delete m;
+		}
 	}
-  }
 
-  return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
